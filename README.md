99aedca41a075b4cbc7d35b806ea31fd2a978e1d
Rolin Blake <rolinmblake@gmail.com>
2024-03-25 18:45:42 -0500

    simple UI, no client sockets yet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Simple browser GUI application using the C++ Win32 API
+
+Compile and run: g++ src/main.cpp -o src/main && ./src/main
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+  WNDCLASSW wc = {0};
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hInstance = hInst;
+  wc.lpszClassName = L"cppBrowser";
+  wc.lpfnWndProc = WindowProcedure;
+  if (!RegisterClassW(&wc)) {
+    return -1;
+  }
+  CreateWindowW(L"cppBrowser", L"cpp-browser", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+  MSG msg = {0};
+  while (GetMessage(&msg, NULL, NULL, NULL)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+  switch (msg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    default:
+      return DefWindowProcW(hWnd, msg, wp, lp);
+  }
+}
