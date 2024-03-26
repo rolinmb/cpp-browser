a77c632033c273888573bce7b3c44ccdaedfaa9f
Rolin Blake <rolinmblake@gmail.com>
2024-03-25 19:11:30 -0500

    added button

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
+void buildGui(HWND);
+
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
   WNDCLASSW wc = {0};
   wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
@@ -12,7 +14,10 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
   if (!RegisterClassW(&wc)) {
     return -1;
   }
-  CreateWindowW(L"cppBrowser", L"cpp-browser", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+  CreateWindowW(L"cppBrowser", L"cpp-browser",
+    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    100, 100, 500, 500,
+    NULL, NULL, NULL, NULL);
   MSG msg = {0};
   while (GetMessage(&msg, NULL, NULL, NULL)) {
     TranslateMessage(&msg);
@@ -23,10 +28,20 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
   switch (msg) {
+	case WM_CREATE:
+	  buildGui(hWnd); 
+	  break;	  
     case WM_DESTROY:
       PostQuitMessage(0);
       break;
     default:
       return DefWindowProcW(hWnd, msg, wp, lp);
   }
+}
+
+void buildGui(HWND hWnd) {
+  CreateWindowW(L"BUTTON", L"Press to request the server.",
+    WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+	10, 10, 200, 30,
+	hWnd, NULL, NULL, NULL);
 }
